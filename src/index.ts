3e6ee1418a5fbc9f03e00fb7a73022630dfc6774
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-10 19:40:12 -0300

    update: Atualizando numero de porta

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import App from 'App';
 import Server from 'Server';
 import express from 'express';
 
-const server: Server = new Server(express(), 3000);
+const server: Server = new Server(express(), Number(process.env.PORT));
 const app: App = new App(server);
 app.init();
