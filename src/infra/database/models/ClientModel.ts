186056420cdb502f7edcb58aed97028b983df896
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-11 19:27:05 -0300

    new: entidade modelo da classe Client

---
+++ src/infra/database/models/ClientModel.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export default class ClientModel {
+      @PrimaryGeneratedColumn('uuid')
+      client_id!: string;
+
+      @Column('varchar')
+      nome!: string;
+
+      @Column('varchar')
+      sobrenome!: string;
+
+      @Column('smallint')
+      idade!: number;
+
+      @Column('varchar')
+      email!: string;
+
+      @Column('varchar')
+      senha!: string;
+
+      @Column('varchar')
+      role!: string;
+
+      @Column('timestamp')
+      created_at!: Date;
+
+}
