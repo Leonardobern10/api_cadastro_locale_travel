a854375c923e58d3475f35462af0d494f63e214c
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-11 19:28:03 -0300

    update: atualização das classes injetadas DI

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,13 +1,18 @@
 import { Application } from 'express';
 import 'dotenv/config';
+import ClientRouter from 'routes/ClientRouter';
+import ClientService from 'services/ClientService';
 
 export default class Server {
     private app: Application;
     private port: number;
+    private clientRouter: ClientRouter;
 
-    constructor(app: Application, port: number) {
+    constructor(app: Application, port: number, router: ClientRouter) {
         this.app = app;
         this.port = port;
+        this.clientRouter = router;
+        this.app.use('/clients', this.clientRouter.getRouter())
     }
 
     public getApp() {
