e0105232ca2940c8c34ecced8d064f04611c1490
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-11 19:29:05 -0300

    update: instanciando as classes para injeção de dependencia

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import App from 'application/App';
 import Server from 'server/Server';
 import express from 'express';
 import { DataSource } from 'typeorm';
-import { AppDataSource } from 'config/data-source';
+import { AppDataSource } from 'configs/data-source';
+import ClientService from 'services/ClientService';
+import ClientRepository from 'infra/database/repositories/ClientRepository';
+import ClientController from 'controllers/ClientController';
+import ClientRouter from 'routes/ClientRouter';
 
-const server: Server = new Server(express(), Number(process.env.PORT));
 const db: DataSource = AppDataSource;
+const repository: ClientRepository = new ClientRepository(db);
+const service: ClientService = new ClientService(repository);
+const controller: ClientController = new ClientController(service);
+const clientRouter: ClientRouter = new ClientRouter(express(), controller);
+
+
+const server: Server = new Server(express(), Number(process.env.PORT), clientRouter);
 const app: App = new App(server, db);
 app.init();
