4e19297f50f7b517e85fc177802e657d8e3fc8b6
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-11 12:18:46 -0300

    add: inserindo utilização do Datasource para injeção de dependencia

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import App from 'application/App';
 import Server from 'server/Server';
 import express from 'express';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from 'config/data-source';
 
 const server: Server = new Server(express(), Number(process.env.PORT));
-const app: App = new App(server);
+const db: DataSource = AppDataSource;
+const app: App = new App(server, db);
 app.init();
