a5e3d1e9858f5da133f47718dedb438633540d4d
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-10 23:43:53 -0300

    update: atualizando importação

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import App from 'App';
-import Server from 'Server';
+import App from 'application/App';
+import Server from 'application/Server';
 import express from 'express';
 
 const server: Server = new Server(express(), Number(process.env.PORT));
