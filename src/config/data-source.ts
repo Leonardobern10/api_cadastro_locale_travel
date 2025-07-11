428fd008dc25517c757f22fe137137e09bbed4a7
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-11 12:19:30 -0300

    update: recebendo dados de variaveis de ambiente

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,16 +1,18 @@
-import Client from "@entities/Client";
+import ClientModel from "@models/ClientModel";
 import "reflect-metadata";
 import { DataSource } from 'typeorm';
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      database: "locale_travel",
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || "5432"),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
-      entities: [Client],
+      entities: [ClientModel],
       subscribers: [],
       migrations: []
 })
