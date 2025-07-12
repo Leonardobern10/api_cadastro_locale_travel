d7f6561357a98340b1c546c8ab37b7b9020acfe5
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-12 16:12:03 -0300

    fix: modificando a chamada para a camada controller

--- src/controllers/ClientController.ts
+++ src/controllers/ClientController.ts
@@ -1,9 +1,9 @@
-import ClientModel from "infra/database/models/ClientModel";
 import { Request, Response } from "express";
 import ClientService from "services/ClientService";
+import ClientDTO from "application/ClientDTO";
 
 export default class ClientController {
-      private readonly clientService: ClientService;
+      private clientService: ClientService;
 
       constructor(clientService: ClientService) {
             this.clientService = clientService;
@@ -14,13 +14,26 @@ export default class ClientController {
       }
 
       public async saveClient(req: Request, res: Response): Promise<void> {
-            const body = req.body;
+            console.log(req.body)
             try {
-                  const client = await this.clientService.saveClientService(new ClientModel());
+                  const { nome, sobrenome, idade, email, senha, role } = req.body;
+                  const dto = new ClientDTO(nome, sobrenome, idade, email, senha, role)
+                  const client = await this.getClientService().saveClientService(dto);
                   res.status(201).json(client);
             } catch (erro) {
-                  console.log(erro);
                   res.status(400).json({ message: "Erro ao cadastrar" })
+                  throw erro;
             }
+      }
+
+      public async allClients(req: Request, res: Response): Promise<void> {
+            try {
+                  const allClients = await this.clientService.getAllClientsService();
+                  res.status(200).json(allClients);
+            } catch (error) {
+                  console.error(error);
+                  res.status(500).json({ "error": "Erro ao ĺistar usuários!" })
+            }
+
       }
 }
