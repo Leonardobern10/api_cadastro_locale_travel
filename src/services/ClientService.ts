7682dfbe0e69106f47bee241cf553d5929a83959
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-12 16:14:16 -0300

    add: utilização da classe para hash de senhas

--- src/services/ClientService.ts
+++ src/services/ClientService.ts
@@ -1,18 +1,37 @@
+import ClientDTO from "application/ClientDTO";
+import ClientMapper from "application/mappers/ClientMapper";
+import PasswordCrypt from "infra/cripto/PasswordCrypt";
 import ClientModel from "infra/database/models/ClientModel";
 import ClientRepository from "infra/database/repositories/ClientRepository";
 
 export default class ClientService {
       private readonly clientRepository: ClientRepository;
+      private passwordCrypter: PasswordCrypt;
 
       constructor(clientRepository: ClientRepository) {
             this.clientRepository = clientRepository;
+            this.passwordCrypter = new PasswordCrypt();
       }
 
       public getClienteRepository(): ClientRepository {
             return this.clientRepository;
       }
 
-      public saveClientService(client: ClientModel) {
-            return this.getClienteRepository().saveClient(client);
+      public getPasswordCrypt(): PasswordCrypt {
+            return this.passwordCrypter;
+      }
+
+      public setPasswordCrypt(passwordCrypter: PasswordCrypt): void {
+            this.passwordCrypter = passwordCrypter;
+      }
+
+      public async saveClientService(clientDTO: ClientDTO): Promise<ClientModel> {
+            clientDTO.senha = await this.getPasswordCrypt().toCrypt(clientDTO.senha);
+            const model = ClientMapper.toModel(clientDTO);
+            return this.getClienteRepository().saveClient(model);
+      }
+
+      public async getAllClientsService(): Promise<ClientModel[]> {
+            return await this.getClienteRepository().allClients();
       }
 }
