e6c74488383850c4072ad1f72b813cd14368d3ca
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-12 16:11:12 -0300

    update: removendo readonly para hash da senha

---
+++ src/application/ClientDTO.ts
@@ -0,0 +1,26 @@
+import { Roles } from "infra/database/models/Roles";
+
+export default class ClientDTO {
+      public readonly nome: string;
+      public readonly sobrenome: string;
+      public readonly idade: number;
+      public readonly email: string;
+      public senha: string;
+      public readonly role: Roles;
+
+      constructor(
+            nome: string,
+            sobrenome: string,
+            idade: number,
+            email: string,
+            senha: string,
+            role: Roles
+      ) {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.idade = idade;
+            this.email = email;
+            this.senha = senha;
+            this.role = role;
+      }
+}
