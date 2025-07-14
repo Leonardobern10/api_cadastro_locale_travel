35eed8e07f79812c879e2605ba46ebb3cbd2f358
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-07-14 10:48:59 -0300

    update: removendo logging

--- src/configs/data-source.ts
+++ src/configs/data-source.ts
@@ -11,7 +11,6 @@ export const AppDataSource = new DataSource({
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true,
-      logging: true,
       entities: [ClientModel],
       subscribers: [],
       migrations: []
